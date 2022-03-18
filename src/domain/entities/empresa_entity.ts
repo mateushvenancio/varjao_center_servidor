a437a22f919f319429f109eeaf007f49639a8a34
Mateus <38964103+mateushvenancio@users.noreply.github.com>
2022-03-18 11:12:40 -0300

    modelagem

---
+++ src/domain/entities/empresa_entity.ts
@@ -0,0 +1,4 @@
+export default interface EmpresaEntity {
+    id: string | number;
+    nome: string;
+}
--- src/domain/entities/produto_entity.ts
+++ src/domain/entities/produto_entity.ts
@@ -2,4 +2,9 @@ export default interface ProdutoEntity {
     id: string;
     nome: string;
     preco: number;
+    porcentagemDesconto?: number;
+    imagens: string[];
+    descricao: string;
+
+    precoFinal(): number;
 }
